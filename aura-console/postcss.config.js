d9d3f54e694f966bdffecce72c0cd5f3229aad4a
Darren Prince <support@aurasystemsai.com>
2025-12-10 17:41:30 +0000

    Tailwind v4 + keep console styling

--- aura-console/postcss.config.js
+++ aura-console/postcss.config.js
@@ -1,6 +1,10 @@
-export default {
+// aura-console/postcss.config.js
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    "@tailwindcss/postcss": {}
+  }
+};
+
+export default config;
