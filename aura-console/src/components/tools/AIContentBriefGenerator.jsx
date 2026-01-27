f79c94844afbc41e483c6b3db40140d9baa338d2
Darren Prince <support@aurasystemsai.com>
2026-01-27 12:45:09 +0000

    fix: BackButton import error in AIContentBriefGenerator

--- aura-console/src/components/tools/AIContentBriefGenerator.jsx
+++ aura-console/src/components/tools/AIContentBriefGenerator.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BackButton from "./BackButton";
 
 export default function AIContentBriefGenerator() {
   // Flagship UI state
@@ -40,12 +41,9 @@ export default function AIContentBriefGenerator() {
   const handleExport = () => {
     const blob = new Blob([JSON.stringify(outline, null, 2)], { type: 'application/json' });
     const url = URL.createObjectURL(blob);
-      import BackButton from "./BackButton";
     setExported(url);
     setTimeout(() => URL.revokeObjectURL(url), 10000);
   };
-
-          <BackButton />
   // Feedback
   const handleFeedback = async () => {
     if (!feedback) return;
